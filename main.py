8d30c684663ae36a63933c86ccc60633de14c61e
Gyusun Yeom <omniavinco@gmail.com>
2015-11-29 00:27:47 +0900

    Key - sensor merging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 key_distances = pd.read_csv('data/key_distances.csv')
 model_infos = pd.read_csv('data/model.csv')
@@ -19,8 +20,67 @@ def load_save_data(filename):
     data = pd.merge(data, model_infos, how='left', on=['model'])
     return data
 
-key_data = load_key_data('data/v2_corporate/key.csv')
-sensor_data = load_sensor_data('data/v2_corporate/sensor.csv')
+#key_data = load_key_data('data/v2_corporate/key.csv')
+#sensor_data = load_sensor_data('data/v2_corporate/sensor.csv')
 save_data = load_save_data('data/v2_corporate/save.csv')
 
-screen_error_rate_by_size = save_data.groupby('diag').mean()
+screen_error_rate_by_size = save_data.groupby('diag').mean()['error_rate']
+#screen_error_rate_by_size.plot()
+
+sensor_columns = ('acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z',
+                  'mag_x', 'mag_y', 'mag_z', 'azim', 'pitch', 'roll',
+                  'light', 'prox')
+
+class mean_sensor:
+    def __init__(self, itr):
+        self.itr = itr
+        self.prev_val = None
+        self.count = 0
+        self.prev_data = None
+
+    def calc(self, time, data, cur_val):
+        if time < cur_val['time']:
+            return False
+        for k in sensor_columns:
+            data[k] += cur_val[k]
+            self.count += 1
+        return True
+
+    def __call__(self, row):
+        time = row['time']
+        self.count = 0
+        data = {k: 0 for k in sensor_columns}
+        if self.prev_val is not None:
+            if self.calc(time, data, self.prev_val):
+                self.prev_val = None
+
+        if self.prev_val is None:
+            for val in self.itr:
+                if not self.calc(time, data, val[1]):
+                    self.prev_val = val[1]
+                    break
+
+        if self.count == 0 and self.prev_data:
+            data = self.prev_data
+        else:
+            self.prev_data = data
+
+        data['time'] = time
+        return pd.Series(data if self.count == 0 else
+                         {k: data[k] / self.count for k in sensor_columns})
+
+prefix = 'data/v1_original/'
+def key_sensor_merged_data(index):
+    post_fix = '_%d.csv' % index
+    key = load_key_data(prefix + 'key/Key' + post_fix)
+    sensor = load_sensor_data(prefix + 'sensor/Sensor' + post_fix)
+    itr = sensor.iterrows()
+    key = key.merge(key.apply(mean_sensor(itr), axis=1))
+
+    return key, sensor
+
+# sample
+key_sensors = [key_sensor_merged_data(i) for i in range(1,20)]
+
+key_data = pd.concat([k[0] for k in key_sensors])
+sensor_data = pd.concat([v[1] for v in key_sensors])
