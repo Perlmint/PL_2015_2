a0a6fe4e754959f4d456f905c0138944698390b4
box327 <kibox327@gmail.com>
2015-11-28 16:43:30 +0900

    ps -> pd

--- calculateConvenience.py
+++ calculateConvenience.py
@@ -1,19 +1,19 @@
-import pandas as ps
+import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
 
 def calculateConvenience(data):
-    pca_data = ps.DataFrame(data,columns=['WPM','error_rate','years_of_use'])
+    pca_data = pd.DataFrame(data,columns=['WPM','error_rate','years_of_use'])
 
     pca_data['error_rate'] = 1 - pca_data['error_rate']
     
     pca = PCA(1)
     x_pca = pca.fit_transform(pca_data)
 
-    s = ps.DataFrame(x_pca,columns=['Convenience'])
+    s = pd.DataFrame(x_pca,columns=['Convenience'])
 
     
-    result = ps.concat([data,s],axis=1)
+    result = pd.concat([data,s],axis=1)
 
     return result
 
