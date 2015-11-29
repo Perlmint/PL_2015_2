2a97c9271c2990a419f40ce5133150273a50a8e0
box327 <kibox327@gmail.com>
2015-11-30 06:50:47 +0900

    corr 연산 버그 수정...

--- calculateCorrelation.py
+++ calculateCorrelation.py
@@ -8,16 +8,24 @@ def calculateCorrelation(save,plt):
 
     size = []
     for i, r in save.iterrows():
-        size.append(r['width'] * r['height'] / 10000)
+        size.append(r['width'] * r['height'] / 20000)
 
-    data_cov = np.corrcoef(save['Convenience'],size)
+    save['size'] = size
 
-    vmax = data_cov.max()
+    data = pd.DataFrame(save,columns=['Convenience','size'])
+
+    data_cov = data.corr()
+
+    vmax = data_cov.values.max()
+
+    print data_cov
 
     fig, ax = plt.subplots()
     ax.xaxis.tick_top()
 
-    plt.imshow(data_cov, interpolation='nearest', vmin=-vmax,vmax=vmax, cmap=plt.cm.RdBu_r)
+    plt.imshow(data_cov.values, interpolation='nearest', vmin=-vmax,vmax=vmax, cmap=plt.cm.RdBu_r)
 
+    plt.xticks(range(0,2))
+    plt.yticks(range(0,2))
     
-    
+    return data_cov
